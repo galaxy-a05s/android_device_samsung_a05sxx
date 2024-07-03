b447c060b9e1722b95fbddd3da60059ec0dc530c
Jamie Falconer <mrfluffyoven@gmail.com>
2024-07-03 13:24:10 +0100

    update properties > A057G

--- twrp_a05s.mk
+++ twrp_a05s.mk
@@ -12,15 +12,15 @@ $(call inherit-product, device/samsung/a05s/device.mk)
 PRODUCT_DEVICE := a05s
 PRODUCT_NAME := twrp_a05s
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A057F
+PRODUCT_MODEL := SM-A057G
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a05snsdxx-user 13 TP1A.220624.014 A057FXXU4CXF3 release-keys"
+    PRIVATE_BUILD_DESC="a05sxx-user 13 TP1A.220624.014 A057GXXS3CXF1 release-keys"
 
-BUILD_FINGERPRINT := samsung/a05snsdxx/a05s:13/TP1A.220624.014/A057FXXU4CXF3:user/release-keys
+BUILD_FINGERPRINT := samsung/a05sxx/a05s:13/TP1A.220624.014/A057GXXS3CXF1:user/release-keys
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
