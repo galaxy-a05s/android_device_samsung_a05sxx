c8f3e8f8abc63d402d169009ccfa292aa47f2892
MrFluffyOven <mrfluffyoven@gmail.com>
2024-12-04 09:26:04 +0000

    fix twrp_a05s.mk error

--- twrp_a05s.mk
+++ twrp_a05s.mk
@@ -1,4 +1,4 @@
-﻿#
+#
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
@@ -20,3 +20,10 @@ PRODUCT_NAME := twrp_a05s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A057G
 PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a05sxx-user 13 TP1A.220624.014 A057GXXS5CXJ2 release-keys"
+
+BUILD_FINGERPRINT := samsung/a05sxx/a05s:13/TP1A.220624.014/A057GXXS5CXJ2:user/release-keys
