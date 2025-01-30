e11c144c4688b739cee0b7e21986cbd69bc296bf
SavedByLight <mrfluffyoven@gmail.com>
2025-01-30 16:34:26 +0000

    minimise modules and load in device tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,10 @@ TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 
+
+TW_LOAD_VENDOR_MODULES := true
+TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/vendor/lib/modules)\")
+
 # Inherit from common tree
 include device/samsung/bengal-common/BoardConfigCommon.mk
 
