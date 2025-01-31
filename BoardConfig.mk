a22839cafda9ebe0a8c70aa7176a571044bebe4e
SavedByLight <mrfluffyoven@gmail.com>
2025-01-31 06:43:16 +0000

    for now don't load modules from vendor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,8 +14,8 @@ TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 
 
-TW_LOAD_VENDOR_MODULES := true
-TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/vendor/lib/modules)\")
+#TW_LOAD_VENDOR_MODULES := true
+#TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/vendor/lib/modules)\")
 
 # Inherit from common tree
 include device/samsung/bengal-common/BoardConfigCommon.mk
